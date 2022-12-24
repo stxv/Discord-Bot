061772769c1276f71f34bedef9f495dbfe68864f
Steven Tran <stevenhuyt@gmail.com>
2022-12-24 02:12:25 -0500

    Allow the bot to come online and be functional while allowing it to respond to a message

--- botprogram.py
+++ botprogram.py
@@ -1,9 +1,19 @@
 import discord
+from Private.config import token
 # client instance allows interaction with discord API
-client = discord.Client()
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 # discord.py revolves around the concept of events. 
 # An event is something you listen to and then respond to.
 # https://discordpy.readthedocs.io/en/stable/api.html#discord-api-events 
+#Let's us know if the bot is online by printing a message
 @client.event
 async def on_ready():
-    print("hello")
+    print(F"{client.user} has logged in")
+#Allows the bot to respond to a specific message
+@client.event
+async def on_message(message):
+    if message.content.startswith("X"):
+        await message.channel.send(f"Hello {message.author.display_name}")
+#Allows the bot to function
+client.run(token)
