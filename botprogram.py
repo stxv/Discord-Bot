f3a25c261153d098a2e9725551d90b26697f9137
Steven Tran <stevenhuyt@gmail.com>
2022-12-24 15:34:12 -0500

    Add basic moderation commands to bot such as deleting links and any undesired words within a list

--- botprogram.py
+++ botprogram.py
@@ -11,9 +11,21 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(F"{client.user} has logged in")
 #Allows the bot to respond to a specific message
+badwords = ["213", "https://"]
+Links = ["https://", "http://"]
 @client.event
 async def on_message(message):
-    if message.content.startswith("X"):
-        await message.channel.send(f"Hello {message.author.display_name}")
+    if message.author != client.user:
+        if message.content.startswith("X"):
+            await message.channel.send(f"Hello {message.author.display_name}")
+#Basic moderation code that deletes text in a given string
+        for Bwords in badwords:
+            if "G" not in str(message.author.roles) and Bwords in str(message.content.lower()):
+                await message.delete()
+        for links in Links:
+#Deletes links sent from users (INCLUDING USERS WITH ADMIN ROLES)
+            if message.content in Links:
+                await message.delete()
+                await message.channel.send(f"{message.author.mention} Don't send links!")
 #Allows the bot to function
 client.run(token)
