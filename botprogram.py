a2bfbd0a4ec4c2efd797056fb4e02793d7343b87
Steven Tran <stevenhuyt@gmail.com>
2023-01-05 15:02:23 -0500

    Allows the bot to welcome members that join the server

--- botprogram.py
+++ botprogram.py
@@ -5,15 +5,14 @@ from Private.config import token
 from discord.ext import commands
 # client instance allows interaction with discord API
 intents = discord.Intents.all()
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 # discord.py revolves around the concept of events. 
 # https://discordpy.readthedocs.io/en/stable/api.html#discord-api-events 
 
 #Let's us know if the bot is online by printing a message
-@client.event
+@bot.event
 async def on_ready():
-    print(F"{client.user} has logged in")
+    print(F"{bot.user} has logged in")
 #Allows the bot to respond to a specific message
 badwords = ["213"]
 Links = ["https://", "http://"]
@@ -27,9 +26,9 @@ def rand_quote():
     finalQ = Jd["body"] + " -" + Jd["author"]
     return finalQ
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author != client.user:
+    if message.author != bot.user:
         if message.content.startswith("X"):
             await message.channel.send(f"Hello {message.author.display_name}")
 #Basic moderation code that deletes text in a given list
@@ -45,10 +44,17 @@ async def on_message(message):
                 return
         if message.content == ("!stoic"):
             await message.channel.send(rand_quote())
+#Members with kick perms will be able to kick other members
         if message.content.startswith('!kick') and message.author.guild_permissions.kick_members:
             user_mention = message.content.split(" ")[1]
             user = message.guild.get_member(int(user_mention.strip("<@!>")))
             message.channel.send(f"{user} has been kicked")
             await user.kick()
+#Welcomes members to server, this has to be coupled with the server settings, change the system message channel to the designated welcome channel
+@bot.event
+async def on_member_join(member):
+    # Send the message to a designated welcome channel
+    channel = bot.get_channel(1057700425140285560)
+    await channel.send(f"Welcome to the server, {member.mention}!")
 #Allows the bot to function
-client.run(token)
+bot.run(token)
